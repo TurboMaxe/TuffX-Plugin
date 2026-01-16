b386527f37c1b8f4ab272ec3d02c4211c42eabc9
turbomaxee <137450788+TurboMaxe@users.noreply.github.com>
2026-01-16 18:55:30 +0500

    no

--- src/main/java/tf/tuff/API/API.java
+++ src/main/java/tf/tuff/API/API.java
@@ -1,12 +1,13 @@
 package tf.tuff.API;
 
+import tf.tuff.ChunkSectionKey;
+
 public class API {
 
-
-
-
+public ChunkSectionKey getChunkSectionKey() {
+  return ChunkSectionKey; 
 
 
   
-  
+     }
 }
