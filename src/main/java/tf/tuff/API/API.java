1937c5b3430e8131e451fe13ca4fe2ea15283bca
turbomaxee <137450788+TurboMaxe@users.noreply.github.com>
2026-01-16 19:25:22 +0500

    imports and new methods

--- src/main/java/tf/tuff/API/API.java
+++ src/main/java/tf/tuff/API/API.java
@@ -1,13 +1,34 @@
 package tf.tuff.API;
 
 import tf.tuff.ChunkSectionKey;
+import tf.tuff.LegacyBlockIds;
+import tf.tuff.TuffX;
 
+
+/* TuffX API for any addons 
+ * author@turbomaxe
+ * since 1/16/2026
+ */
 public class API {
 
-public ChunkSectionKey getChunkSectionKey() {
-  return ChunkSectionKey; 
-
-
+  public ChunkSectionKey getChunkSectionKey() {
+    return ChunkSectionKey; 
+    }
+  public static Map<String, Integer> getBlockIDMap() {
+    return Collections.unmodifiableMap(BlockRegistry.BLOCK_ID_MAP);
+    }
   
-     }
+  public static Integer getBlockId(String name) {
+    return BlockRegistry.BLOCK_ID_MAP.get(name);
+    }
+
+   public static AuctionHouseAPI getInstance() {
+        if (instance == null) {
+            throw new IllegalStateException(
+    }   
+     public static TuffX getInstance() {
+     return instance;
+    }
 }
+
+
