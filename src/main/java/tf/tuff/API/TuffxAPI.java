efc16e3d4a9821afdee080578f69fb9ecf692e85
turbomaxee <137450788+TurboMaxe@users.noreply.github.com>
2026-01-18 02:18:19 +0500

    rewrite
    fix code erorr

--- src/main/java/tf/tuff/API/TuffxAPI.java
+++ src/main/java/tf/tuff/API/TuffxAPI.java
@@ -1,11 +1,18 @@
-package tf.tuff.TuffxAPI;
+package tf.tuff.API.TuffXAPI;
 
+import org.bukkit.plugin.java.JavaPlugin;
 import tf.tuff.ChunkSectionKey;
 import tf.tuff.LegacyBlockIds;
+import tf.tuff.ServerRegistry;
 import tf.tuff.TuffX;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.OptionalInt;
+import java.util.UUID;
+
+import static tf.tuff.LegacyBlockIds.BLOCK_ID_MAP;
+import static tf.tuff.LegacyBlockIds.unmapped;
 
 /* TuffX API for any addons 
  * author@turbomaxe
@@ -14,25 +21,36 @@ import java.util.Map;
 public class TuffxAPI {
     private static TuffxAPI instance = null;
 
-  public static final String version = "1.2.0";
-  
-   public TuffxAPI() {}
+    public static final String version = "1.2.0";
 
-  public static ChunkSectionKey getChunkSectionKey() {
-    return new ChunkSectionKey(); 
+    public TuffxAPI() {
+    }
+
+    public static ChunkSectionKey getChunkSectionKey(
+            UUID player,
+            String world,
+            int x,
+            int z,
+            int y
+    ) {
+        return new ChunkSectionKey(player, world, x, z, y);
     }
 
     public static TuffxAPI getInstance() {
         if (instance == null) {
             throw new IllegalStateException(
                     "The TuffX API is not initialized yet!"
-                    );
+            );
         }
         return instance;
     }
-  
-  public static Map<String, Integer> getBlockIDMap() {
-    return Collections.unmodifiableMap(BlockRegistry.BLOCK_ID_MAP);
+
+    public ServerRegistry getServerRegistry(JavaPlugin pl, String registryUrl, String serverAddr) {
+        return new ServerRegistry(pl, registryUrl, serverAddr);
+    }
+
+    public static Map<String, Integer> getBlockIDMap() {
+        return Collections.unmodifiableMap(BLOCK_ID_MAP);
     }
 
     public static void setInstance(TuffxAPI newInstance) {
@@ -41,16 +59,18 @@ public class TuffxAPI {
         }
         instance = newInstance;
     }
-  
-  public static Integer getBlockId(String name) {
-    return BlockRegistry.BLOCK_ID_MAP.get(name);
+
+    public static Integer getBlockName(int id) {
+        return BLOCK_ID_MAP.get(id);
     }
 
-  public static String getVersion() {
-    return version;    
+    public static String getVersion() {
+        return version;
     }
-  
-}   
 
+    public static int getBlockID(String name) {
+        return BLOCK_ID_MAP.getOrDefault(name, 1);
+    }
+}
 
 
